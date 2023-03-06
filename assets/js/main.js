8457066fab94daa50012dc00aac9780ef9b3bed6
rayhan <aburayhandh@gmail.com>
2023-03-06 20:35:09 +0600

    mobile version ux completed with cart function

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,13 +67,55 @@ let newSwiper = new Swiper(".new-swiper", {
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SHOW CART ===============*/
+const cart = document.getElementById('cart'),
+    cartShop = document.getElementById('cart-shop'),
+    cartClose = document.getElementById('cart-close')
 
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if (cartShop) {
+    cartShop.addEventListener('click', () => {
+        cart.classList.add('show-cart')
+    })
+}
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+if (cartClose) {
+    cartClose.addEventListener('click', () => {
+        cart.classList.remove('show-cart')
+    })
+}
 
 /*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById('theme-button')
